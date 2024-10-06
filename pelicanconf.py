cc9b34476653c11a74b408d22888e22d61c53564
miguelcastroe <135630596+miguelcastroe@users.noreply.github.com>
2024-10-06 17:59:43 -0500

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,38 +4,38 @@ AUTHOR = 'Miguel Castro'
 SITENAME = 'Creativity Evolution'
 SITEURL = ''
 
+# Path to content
 PATH = 'content'
 
+# Timezone and language settings
 TIMEZONE = 'UTC'
-
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
+# Feed generation (disable during development)
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# Blogroll (Links section)
+LINKS = (
+    ('Pelican', 'https://getpelican.com/'),
+    ('Python.org', 'https://www.python.org/'),
+    ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Social widget (Add your social media links here)
+SOCIAL = (
+    ('GitHub', 'https://github.com/miguelcastroe'),
+    # Add more social links here
+)
 
-DEFAULT_PAGINATION = False
+# Default settings
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
-# Extra path metadata to include data and images
-EXTRA_PATH_METADATA = {
-    'content/data/milestones.json': {'path': 'data/milestones.json'},
-    'content/images/milestones_chart.png': {'path': 'images/milestones_chart.png'},
-}
-
-# Additional configurations can be added here
+# Output path for generated site
+OUTPUT_PATH = 'output/'
